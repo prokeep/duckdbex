a49809a7166eeef8b9b3adf9ba125be6dc5763be
Jason LeCount <jason.lecount@prokeep.com>
2026-05-15 15:20:05 -0700

    Force checksum creation for now

---
+++ checksum.exs
@@ -0,0 +1,8 @@
+%{
+  "duckdbex-nif-2.17-aarch64-apple-darwin-0.4.0-prokeep.2.tar.gz" =>
+    "sha256:9e9821b498d2f6346c02adb238013277823482e16f3c04be9e13b5164bd6240b",
+  "duckdbex-nif-2.17-x86_64-apple-darwin-0.4.0-prokeep.2.tar.gz" =>
+    "sha256:7ac2b1f042dcbc163efa005a7e2c3dd652863cca3d2ba229910e68aff61fd05a",
+  "duckdbex-nif-2.17-x86_64-linux-musl-0.4.0-prokeep.2.tar.gz" =>
+    "sha256:7b3ca3842ef3a6b3fe42cba6eeac374e3d098d6bcfca509906f9c02496836ed9"
+}
